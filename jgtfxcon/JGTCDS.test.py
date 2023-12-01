b57befb53de3b2b0411014ff16e461b1e363fb63
jgi <jgi@jgwill.com>
2023-11-30 22:01:22 -0500

    clrean

--- jgtfxcon/JGTCDS.test.py
+++
@@ -1,8 +0,0 @@
-from . import JGTCDS as cds
-
-instrument="EUR/USD"
-timeframe = "D1"
-c=cds.create(instrument,timeframe,nb2retrieve=3000,stayConnected=False,quiet=True)
-print(c)
-c.to_csv("../test/output-cds-231109a.csv")
-
