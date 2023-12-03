8895cdc3e2604147cb2275cbd05f760e137198dc
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-03 13:23:51 -0500

    fix

--- scripts/_version_sed.sh
+++ scripts/_version_sed.sh
@@ -3,8 +3,8 @@ pversion=$1
 nversion=$2
 cdir=$(pwd)
 
-if [ -e "jgtpy/.flag" ] ;then 
-cd jgtpy
+if [ -e "jgtfxcon/.flag" ] ;then 
+cd jgtfxcon
 for f in $(ls *);do if [ -f "$f" ]; then sed -i 's/'$pversion'/'$nversion'/g' $f &>/dev/null;fi;done
 cd $cdir
 cd test
@@ -12,7 +12,7 @@ for f in $(ls *);do if [ -f "$f" ]; then sed -i 's/'$pversion'/'$nversion'/g' $f
 cd $cdir
 for f in $(ls *);do if [ -f "$f" ]; then sed -i 's/'$pversion'/'$nversion'/g' $f &>/dev/null;fi;done
 else
-	echo " MUST USE VM  To have jgtpy Mounted: use dkrun "
+	echo " MUST USE VM  To have jgtfxcon Mounted: use dkrun "
 fi
 else
 	echo "Must supply old and new version number"
