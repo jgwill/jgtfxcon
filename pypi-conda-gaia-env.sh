fb01aaa7dbafae13a4b15e395b0372acbd6d7cb9
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-09 10:45:37 -0500

    chg

--- pypi-conda-gaia-env.sh
+++ pypi-conda-gaia-env.sh
@@ -1,4 +1,6 @@
+
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
+if [ "$1" == "" ]; then
 conda activate jgtpy-pypi && sleep 1 && echo "... Waiting before we install the fresh package $cversion" && sleep 1 && echo "..." && \
 	pip uninstall -y jgtfxcon && sleep 2 && \
 	echo "pip install jgtfxcon==$cversion" && sleep 1 && pip install -U jgtfxcon==$cversion && \
@@ -7,3 +9,7 @@ conda activate jgtpy-pypi && sleep 1 && echo "... Waiting before we install the 
 	cd test && ls *py *sh 
 	(sleep 10;pip install -U jgtfxcon==$cversion &>/dev/null) &
 	
+else 
+	echo "Bypassed install and prep testing :  "
+	echo "        pip install -U jgtfxcon==$cversion"
+fi
