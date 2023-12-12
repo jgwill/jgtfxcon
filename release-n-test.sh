4273b02f536010d46e6d30f6cf74ae27c29465bd
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-12 03:41:23 -0500

    fix:ver

--- release-n-test.sh
+++ release-n-test.sh
@@ -3,6 +3,8 @@
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
 
-make dist && twine upload dist/*  &&         echo "Bypassed install and prep testing :  " \
-	&&    echo "        pip install -U jgtfxcon==$cversion"
+make dist && twine upload dist/* &&         echo "Bypassed install and prep testing :  " && \
+	echo "conda deactivate && pip install --user jgtfxcon"
+
+#	&&    echo "        pip install -U jgtfxcon==$cversion"
 #&& sleep 29 &&  . pypi-conda-gaia-env.sh $1
