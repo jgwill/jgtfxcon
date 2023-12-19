2278bb346836555ce07c51d4d4ea6c7bce609895
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-19 02:53:54 -0500

    fix:iprop

--- manual-make-install-dist.sh
+++ manual-make-install-dist.sh
@@ -1,5 +1,6 @@
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 make dist  && \
+	pip uninstall jgtfxcon -y && \
   pip install --user dist/jgtfxcon-$cversion-py3-none-any.whl  && \
   jgtfxcli -i "USD/CAD" -t "m15" -o  -v 2 && echo "--------TST1 passed " && \
   jgtfxcli -i "USD/CAD,EUR/USD,SPX500" -t "m15,m5,H1,H4,D1,W1" -c 1500 -o -v 2
