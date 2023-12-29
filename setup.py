d1ad15006dc5173299a8a5f423595bf85ec915fb
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-28 22:28:54 -0500

    chg:dep

--- setup.py
+++ setup.py
@@ -13,14 +13,7 @@ from jgtfxcon import __version__ as version
 INSTALL_REQUIRES = [
     'pandas>=0.25.1',
     'python-dotenv>=0.19.2',
-    'jgtapy>=1.9.12',
     'jgtutils',
-    'dropbox>=11.36.2',
-    'packaging',
-    'tqdm',
-    'anyio',
-    'aiohttp',
-    'PyYAML',
     'flask'
 ]
 
