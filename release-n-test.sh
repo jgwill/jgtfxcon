180c1b6f614844c365282453be80eeda858fe141
JGWill <jgi@jgwill.com>
2024-02-25 09:56:20 -0500

    sh

--- release-n-test.sh
+++ release-n-test.sh
@@ -5,7 +5,7 @@ git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git pu
 
 make dist && twine upload dist/* &&         echo "Bypassed install and prep testing :  " && \
 	echo "conda deactivate && pip install --user -U jgtfxcon" && \
-	(conda deactivate && conda deactivate && conda deactivate && pip install --user -U jgtfxcon) &> /dev/null
+	(conda deactivate && conda deactivate && conda deactivate && pip install --user -U jgtfxcon;pip install --user jgtfxcon==$cversion) &> /dev/null
 
 #	&&    echo "        pip install -U jgtfxcon==$cversion"
 #&& sleep 29 &&  . pypi-conda-gaia-env.sh $1
