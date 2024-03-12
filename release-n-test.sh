2f626d428e5cc17124209430111821801f798257
JGWill <jgi@jgwill.com>
2024-03-12 10:16:33 -0400

    v0.4.0

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,7 +1,7 @@
 
 . scripts/version-patcher.sh
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
-git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
+git commit . -m "v$cversion" ;git tag "$cversion" && git push --tags ;git push &> /dev/null 
 
 make dist && twine upload dist/* &&         echo "Bypassed install and prep testing :  " && \
 	(echo "conda deactivate && pip install --user -U jgtfxcon";echo pip install --user jgtfxcon==$cversion) && \
