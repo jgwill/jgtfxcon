e5231e0c9a736ba7c55e121797d0a1c45e6c5008
JGWill <jgi@jgwill.com>
2024-03-12 12:49:15 -0400

    fix:dep

--- release-n-test.sh
+++ release-n-test.sh
@@ -4,8 +4,9 @@ cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion" ;git tag "$cversion" && git push --tags ;git push &> /dev/null 
 
 make dist && twine upload dist/* &&         echo "Bypassed install and prep testing :  " && \
-	(echo "conda deactivate && pip install --user -U jgtfxcon";echo pip install --user jgtfxcon==$cversion) && \
-	(conda deactivate && conda deactivate && conda deactivate && pip install --user -U jgtfxcon;pip install --user jgtfxcon==$cversion) &> /dev/null
+	(echo "conda deactivate && pip install --user -U jgtfxcon";echo pip install --user jgtfxcon==$cversion) 
+	#&& \
+	#(conda deactivate && conda deactivate && conda deactivate && pip install --user -U jgtfxcon;pip install --user jgtfxcon==$cversion) &> /dev/null
 
 #	&&    echo "        pip install -U jgtfxcon==$cversion"
 #&& sleep 29 &&  . pypi-conda-gaia-env.sh $1
