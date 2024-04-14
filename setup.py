1b411fda49362f8d27698142d212623ce3562881
JGWill <jgi@jgwill.com>
2024-04-14 09:01:05 -0400

    dep

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from jgtfxcon import __version__ as version
 INSTALL_REQUIRES = [
     'pandas>=0.25.1',
     'python-dotenv>=0.19.2',
-    'jgtutils>=0.1.58',
+    'jgtutils>=0.1.59',
     'jgtfx2console>=0.4.10',
     'tlid',
     'flask'
