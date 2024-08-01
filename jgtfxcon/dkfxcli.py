2fd5bbc4eb18ccb9d5715e613fd8f8812be1bdc9
JGWill <jgi@jgwill.com>
2024-08-01 14:01:12 -0400

    refactor for future generalization

--- jgtfxcon/dkfxcli.py
+++ jgtfxcon/dkfxcli.py
@@ -16,10 +16,13 @@ def main():
         docker_command.append("-it")
         
 
+    docker_image_repo_tag = "jgwill/jgt:fxcon"
+    cli_to_run_in_container = "jgtfxcli"
+    
     docker_command = docker_command+       [ "--rm",
         "-v", f"{config_path}:/etc/jgt/config.json",
         "-v", f"{data_path}:/data",
-        "jgwill/jgt:fxcon"]
+        docker_image_repo_tag]
 
     # Check if --bash is present in the arguments
     if "--bash" in sys.argv:
@@ -27,7 +30,7 @@ def main():
         # Remove --bash from the arguments list
         sys.argv.remove("--bash")
     else:
-        docker_command.append("jgtfxcli")
+        docker_command.append(cli_to_run_in_container)
     print(f"Running Docker command: {' '.join(docker_command)}")
     # Append all remaining arguments passed to fxcli to the docker command
     docker_command.extend(sys.argv[1:])
