4e6e223be3a83eea772a2e9f2e817bed224b7e0d
JGWill <jgi@jgwill.com>
2024-08-09 18:02:00 -0400

    fix

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,17 +1,10 @@
 #pip install -U jgtutils
 
-. scripts/version-patcher.sh
-cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
-git commit . -m "v$cversion" ;git tag "$cversion" && git push --tags ;git push &> /dev/null 
-
+. scripts/version-patcher.sh && \
+cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}') && \
+(git commit . -m "v$cversion" ;git tag "$cversion" && git push --tags ;git push &> /dev/null) && \
 make dist && twine upload dist/* &&         echo "Bypassed install and prep testing :  " && \
-	(echo "conda deactivate && pip install --user -U jgtfxcon";echo pip install --user jgtfxcon==$cversion) 
-	#&& \
-	#(conda deactivate && conda deactivate && conda deactivate && pip install --user -U jgtfxcon;pip install --user jgtfxcon==$cversion) &> /dev/null
-
-#	&&    echo "        pip install -U jgtfxcon==$cversion"
-#&& sleep 29 &&  . pypi-conda-gaia-env.sh $1
-
+	(echo "conda deactivate && pip install --user -U jgtfxcon";echo pip install --user jgtfxcon==$cversion) && \
 (echo "Building Docker image in +/- 30s...";sleep 33 ;. dkbuild.sh)
 
 #(cd bin/base && dkbuild && dkpush) && echo "Docker image built and pushed to Docker Hub"
