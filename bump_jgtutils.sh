7f538d2a88b30bf90bc1251910866381edb6b6b1
JGWill <jgi@jgwill.com>
2024-08-18 04:14:42 -0400

    fix

--- bump_jgtutils.sh
+++ bump_jgtutils.sh
@@ -1,28 +1,5 @@
 
+. /opt/binscripts/load.sh &>/dev/null
 
-
-# This wont be run by post_dist rather its just a flag to run _bump_jgtutils function
-
-
-pip install --user -U jgtutils
-
-
-oldjgtutilsversion=$(cat pyproject.toml|grep "jgtutils"|tr '>' ' '|tr "'" " "|tr "=" " "|tr "," " "|awk '{print $2}')
-. .env 
-#(conda activate $WS_CONDA_ENV_NAME&>/dev/null;pip install -U jgtutils|tr '(' ' '|tr ')' ' '|grep "jgtutils in"|awk '/jgtutils/{print $7}')
-newjgtutilsversion=$(conda activate $WS_CONDA_ENV_NAME&>/dev/null;pip install -U jgtutils|tr '(' ' '|tr ')' ' '|grep "jgtutils in"|awk '/jgtutils/{print $7}')
-
-# We want to replace jgtutils>=0.4.70 with jgtutils>=0.4.71
-## run if they are different
-if [ "$oldjgtutilsversion" == "$newjgtutilsversion" ]; then
-    echo "No need to update jgtutils version in $WS_CONDA_ENV_NAME package/env"
-else
-
-	sed -i "s/jgtutils>=$oldjgtutilsversion/jgtutils>=$newjgtutilsversion/g" pyproject.toml
-	git add pyproject.toml &>/dev/null
-	git commit -m "auto bump:jgtutils  $oldjgtutilsversion to $newjgtutilsversion"&>/dev/null
-fi
-
-
-#(conda activate baseprod && pip install --user -U jgtutils) &>/dev/null &
+_bump_jgtutils
 
