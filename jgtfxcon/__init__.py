040b1bc230fd4ac301ef4085e19498724dc7ded8
JGWill <jgi@jgwill.com>
2024-08-28 17:58:19 -0400

    bump:dev

--- jgtfxcon/__init__.py
+++ jgtfxcon/__init__.py
@@ -57,7 +57,7 @@ class NotCompatibleException(Exception):
 
 #from jgtfxcon.common_samples import common_samples
 from jgtetl import svc_offset_dt_by_tf as etl_offset_dt_by_tf,offsetdt as etl_offsetdt
-version='0.6.95'
+version='0.6.96'
 
 # if platform.system() == 'Linux':
 #   #sys.path.append(os.path.abspath('./'))
